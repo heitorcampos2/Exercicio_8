c4ce893edb74f01c1532798f835fdbb39ba1eb73
heitorcampos2 <heitormtb12@gmail.com>
2019-06-03 19:16:39 -0300

    v0

---
+++ src/Q_01e02/Vaca.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Q_01e02;
+
+/**
+ *
+ * @author HeiThor
+ */
+public class Vaca {
+    
+}
