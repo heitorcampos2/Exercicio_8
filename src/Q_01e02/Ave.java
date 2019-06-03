c1dc3a53349a595ca10ae1e1d4c5b2978120aed9
heitorcampos2 <heitormtb12@gmail.com>
2019-06-03 14:30:11 -0300

    v0

---
+++ src/Q_01e02/Ave.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Q_01e02;
+
+/**
+ *
+ * @author HeiThor
+ */
+public class Ave extends Animal{
+    private String voa;
+    /////////////////////////
+    public String getVoa(){
+        return this.voa;
+    }
+    
+    public void setVoa(String v){
+        this.voa = v;
+    }
+   //////////////////////////////
+   public boolean voa(){
+       
+   }
+   
+   public void talk(String t){
+       System.out.println("piu-piu");
+   }
+}
