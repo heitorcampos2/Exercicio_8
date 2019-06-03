38d6e7584175e0d33de2a49c201b205b199956d4
heitorcampos2 <heitormtb12@gmail.com>
2019-06-03 19:56:33 -0300

    v0

---
+++ src/Q_03/Empregado.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Q_03;
+
+/**
+ *
+ * @author HeiThor
+ */
+public class Empregado extends Pessoa {
+    private String codigoSetor;
+    private double salarioBase;
+    
+    
+    
+}
