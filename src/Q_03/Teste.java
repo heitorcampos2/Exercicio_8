f2d2ca949ccd3c0afa10f848e6b6ba36bb64363a
heitorcampos2 <heitormtb12@gmail.com>
2019-06-03 22:15:46 -0300

    v0

---
+++ src/Q_03/Teste.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Q_03;
+
+/**
+ *
+ *Crie uma classe de teste que contenha um main e 
+ * crie uma pessoa, um fornecedor, um empregado, um administrador e um operário. 
+ * 
+ * Chame os métodos de cada classe (que não sejam os gets e sets). 
+ * 
+ * @author ifnmg
+ */
+public class Teste {
+    public void main(String[] args){
+        
+    }
+}
