1033e9e46b32b1301417f61a8a1e3f20ac5cae3e
heitorcampos2 <heitormtb12@gmail.com>
2019-06-03 21:35:30 -0300

    v0

--- src/Q_03/Empregado.java
+++ src/Q_03/Empregado.java
@@ -9,10 +9,53 @@ package Q_03;
  *
  * @author HeiThor
  */
+/*
+Crie uma classe Empregado, que herde da classe Pessoa. 
+
+Atributos:
+codigoSetor, salarioBase (vencimento base) e imposto (porcentagem retida dos
+impostos). 
+
+Métodos: calcularSalario (leve em consideração o salario base e o
+imposto). 
+
+Crie um construtor para setar todos os atributos da classe
+Empregado. Crie os gets e sets.
+*/
 public class Empregado extends Pessoa {
     private String codigoSetor;
     private double salarioBase;
+    private double imposto;
     
+    public Empregado(String n, String e, String t,String c,double s,double i){
+    super(n,e,t);
+    setCodigoSetor(c);
+    setSalarioBase(s);
+    setImposto(i);
+    }
     
+    public String getCodigoSetor(){
+        return this.codigoSetor;
+    }
+    
+    public double getSalarioBase(){
+        return this.salarioBase;
+    }
+    
+    public double getImposto(){
+        return this.imposto;
+    }
+    
+    public void setCodigoSetor(String c){
+        this.codigoSetor = c;
+    }
+    
+    public void setSalarioBase(double s){
+        this.salarioBase = s;
+    }
+    
+    public void setImposto(double i){
+        this.imposto = i;
+    }
     
 }
